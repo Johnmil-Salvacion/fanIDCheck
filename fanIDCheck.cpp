e3c41308e3b4eba89f3a10a5dd74b266aba82e7a
Johnmil-Salvacion <jhanaizah@gmail.com>
2024-09-10 09:16:58 +0800

    Add files via upload
    Getting to know each other
    #NameCheck
    #NoToFakeAccounts

---
+++ fanIDCheck.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string name;
+	cout<<"hello sT4lk3r, Who r u?"<<endl;
+	cout<<"Name mo po: ";
+	cin>>name;
+	cout<<"hello "<<name<<". Ur my fann now <3"<<endl;
+	return 0;
+}
